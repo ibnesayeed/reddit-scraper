a594c41e55aec539c1d9145dbdc8d286cb750f1f
Sawood Alam <ibnesayeed@gmail.com>
2015-07-16 12:22:29 -0400

    Not running curl in background to avoid lots of defunct processes on the cost of additional delay and changed the output folder naming based on the content not the time

--- reddit_scraper.rb
+++ reddit_scraper.rb
@@ -4,10 +4,10 @@ start_time   = Time.now.utc.to_i
 start_at     = (ARGV.shift || 1  ).to_i
 blocks_count = (ARGV.shift || 10 ).to_i
 block_size   = (ARGV.shift || 100).to_i
-type_prefix  =  ARGV.shift || "t3"
-reddit_user  =  ARGV.shift || "/u/ibnesayeed"
-output_dir   =  ARGV.shift || "output/#{type_prefix}/#{start_time}"
 end_at       = start_at + block_size * blocks_count - 1
+type_prefix  = ARGV.shift || "t3"
+reddit_user  = ARGV.shift || "/u/ibnesayeed"
+output_dir   = ARGV.shift || "output/#{type_prefix}/#{start_at}-#{end_at}"
 version_str  = "v1.0.#{start_time}"
 user_agent   = "linux-docker:ruby-reddit-#{type_prefix}-scraper:#{version_str} (by #{reddit_user})"
 throttle     = 2
@@ -18,6 +18,6 @@ throttle     = 2
   block_end = block_start + block_size - 1
   ids = (block_start..block_end).map{|i| "t3_#{i.to_s(36)}"}.join ","
   uri = "https://www.reddit.com/by_id/#{ids}.json?limit=#{block_size}"
-  `curl -i -s -A "#{user_agent}" "#{uri}" > #{output_dir}/#{block_start}-#{block_end}.http &`
+  `curl -i -s -A "#{user_agent}" "#{uri}" > #{output_dir}/#{block_start}-#{block_end}.http`
   sleep throttle
 end
